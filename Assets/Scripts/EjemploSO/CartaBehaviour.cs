671ec7268c7e4ca36219fb4720b784bb58d62886
Guillermo Rivas <grivas@pop-os.localdomain>
2023-10-10 18:55:40 -0600

    clase de oct 10

---
+++ Assets/Scripts/EjemploSO/CartaBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Assertions;
+
+public class CartaBehaviour : MonoBehaviour
+{
+    [SerializeField] private CartaScriptableObject _datos;
+
+    [SerializeField] private TMP_Text _nombreText;
+    [SerializeField] private TMP_Text _nivelDeAtaqueText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.IsNotNull(_datos, "DATOS NO PUEDEN SER NULO EN CARTA BEHAVIOUR");
+        Assert.IsNotNull(_nombreText, "TEXT DE NOMBRE NO PUEDE SER NULO EN CARTA BEHAVIOR");
+        Assert.IsNotNull(_nivelDeAtaqueText, "TEXT DE NIVEL DE ATAQUE NO PUEDE SER NULO EN CARTA BEHAVIOR");
+        _nombreText.text = _datos._nombre;
+        _nivelDeAtaqueText.text = _datos._nivelDeAtaque.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EjemploSO/CartaScriptableObject.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Carta", menuName="Cartas/CartaChida")]
+public class CartaScriptableObject : ScriptableObject
+{
+
+    public string _nombre;
+    public float _nivelDeAtaque;
+    
+}
